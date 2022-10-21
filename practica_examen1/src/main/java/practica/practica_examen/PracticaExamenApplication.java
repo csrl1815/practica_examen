3aa548847247f9d866d7d6c9514ed0cbbb9704c3
csrl1815 <50893788+csrl1815@users.noreply.github.com>
2022-10-20 19:17:31 -0700

    primer commit

---
+++ practica_examen1/src/main/java/practica/practica_examen/PracticaExamenApplication.java
@@ -0,0 +1,13 @@
+package practica.practica_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaExamenApplication.class, args);
+	}
+
+}
